8d4d867ece6cd2cac4bc112264c076f18ee61d54
Tomás <tomas.benitez.silva@gmail.com>
2021-11-23 00:04:42 -0300

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fcadb871c1ebe26f52314d9db4683339'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '157f7162-1b25-4f46-8402-2f43873c971e'
+};
